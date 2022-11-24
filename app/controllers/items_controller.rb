c18ecb6a4b7bfd3c9ede07bccea34d62c746851e
jerryjohns <jerry.johns@student.moringaschool.com>
2022-11-24 08:12:43 +0300

    commit tree 062a60e8090a9d4238b32ddd0a14a8c2dc04c315

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+ def index
+        items = Item.all
+        render json: items, except: [:created_at, :updated_at, :user_id], 
+        include: {user: {except: [:created_at, :updated_at]}}
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+ def show
+        user = User.find_by(id: params[:id])
+        render json: user, only: [:id, :username, :city], 
+        include: {items: {only: [:id, :name, :description, :price]}}
+    end
 end
